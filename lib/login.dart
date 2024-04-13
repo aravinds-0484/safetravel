3d8becd22e460a85bded6a3179ff4d2952f5dd36
Krishnaraju06 <krishnapriyaraju07@gmail.com>
2024-04-13 19:32:18 +0530

    copied

--- lib/login.dart
+++ lib/login.dart
@@ -1,53 +1,422 @@
 import 'package:flutter/material.dart';
-//import 'package:get/route_manager.dart';
+import 'package:flutter/services.dart';
+import 'package:safetravel/page2.dart';
 
-class MyLogin extends StatefulWidget {
-  const MyLogin({super.key});
+class MyLogin extends StatelessWidget {
+  const MyLogin({Key? key}) : super(key: key);
 
-  @override
-  _MyLoginState createState() => _MyLoginState();
-}
-
-class _MyLoginState extends State<MyLogin> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Center(
-      child: Scaffold(
-        body: Center(
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
                 width: width * 0.4,
-                height: height * 0.4,
+                height: height * 0.5,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset('LOGO.png', width: 200, height: 150),
-                    const SizedBox(height: 10),
+                    Image.asset(
+                      'LOGO.png',
+                      width: width * 0.4,
+                      height: height * 0.4,
+                    ),
+                    SizedBox(height: height * 0.001),
                     Text("Your safest travel partner"),
                   ],
                 ),
               ),
-
-              // Adjust as needed
               Container(
-                width: 150,
-                child: OutlinedButton(
-                  onPressed: () {},
-                  child: Row(children: [
-                    Image.asset('google.png', width: 20, height: 20),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text("SIGN UP")
-                  ]),
+                width: width * 0.4,
+                height: height * 0.2,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                        keyboardType: TextInputType.emailAddress,
+                        onSaved: (String? value) {},
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        onSaved: (String? value) {},
+                        obscureText: true,
+                      ),
+                      // Add your form fields or other widgets here
+                    ],
+                  ),
                 ),
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  width: 150,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyPage2()),
+                      );
+                    },
+                    child: Row(children: [
+                      SizedBox(width: width * 0.055),
+                      Text("SIGN IN")
+                    ]),
+                  ),
+                ),
+              ),
+              SizedBox(
+                  height:
+                      16), // Add some space between sign-in button and sign-up text
+              SignUpText(
+                onPressed: () {
+                  // Navigate to sign up page or perform any other action
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TextFieldDemo()),
+                  );
+                  print("Sign up tapped!");
+                },
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldDemo extends StatelessWidget {
+  const TextFieldDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Sign Up",
+          style: TextStyle(
+            fontFamily: 'Inter.ttf', // Font size
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(1000, 138, 205, 156),
+      ),
+      body: const TextFormFieldDemo(),
+    );
+  }
+}
+
+class TextFormFieldDemo extends StatefulWidget {
+  const TextFormFieldDemo({Key? key}) : super(key: key);
+
+  @override
+  TextFormFieldDemoState createState() => TextFormFieldDemoState();
+}
+
+class PersonData {
+  String? name = '';
+  String? phoneNumber = '';
+  String? email = '';
+  String password = '';
+}
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+    Key? key,
+    this.restorationId,
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+    this.focusNode,
+    this.textInputAction,
+  }) : super(key: key);
+
+  final String? restorationId;
+  final Key? fieldKey;
+  final String? hintText;
+  final String? labelText;
+  final String? helperText;
+  final FormFieldSetter<String>? onSaved;
+  final FormFieldValidator<String>? validator;
+  final ValueChanged<String>? onFieldSubmitted;
+  final FocusNode? focusNode;
+  final TextInputAction? textInputAction;
+
+  @override
+  State<PasswordField> createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> with RestorationMixin {
+  final RestorableBool _obscureText = RestorableBool(true);
+
+  @override
+  String? get restorationId => widget.restorationId;
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_obscureText, 'obscure_text');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: widget.fieldKey,
+      restorationId: 'password_text_field',
+      obscureText: _obscureText.value,
+      maxLength: 8,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: InputDecoration(
+        filled: true,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        helperText: widget.helperText,
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              _obscureText.value = !_obscureText.value;
+            });
+          },
+          icon: Icon(
+            _obscureText.value ? Icons.visibility : Icons.visibility_off,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFormFieldDemoState extends State<TextFormFieldDemo>
+    with RestorationMixin {
+  PersonData person = PersonData();
+
+  late FocusNode _phoneNumber, _email, _lifeStory, _password, _retypePassword;
+
+  @override
+  void initState() {
+    super.initState();
+    _phoneNumber = FocusNode();
+    _email = FocusNode();
+    _lifeStory = FocusNode();
+    _password = FocusNode();
+    _retypePassword = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _phoneNumber.dispose();
+    _email.dispose();
+    _lifeStory.dispose();
+    _password.dispose();
+    _retypePassword.dispose();
+    super.dispose();
+  }
+
+  @override
+  String get restorationId => 'text_field_demo';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_autoValidateModeIndex, 'autovalidate_mode');
+  }
+
+  final RestorableInt _autoValidateModeIndex =
+      RestorableInt(AutovalidateMode.disabled.index);
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormFieldState<String>> _passwordFieldKey =
+      GlobalKey<FormFieldState<String>>();
+  final _UsNumberTextInputFormatter _phoneNumberFormatter =
+      _UsNumberTextInputFormatter();
+
+  void _handleSubmitted() {
+    final form = _formKey.currentState!;
+    if (!form.validate()) {
+      _autoValidateModeIndex.value =
+          AutovalidateMode.always.index; // Start validating on every change.
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Form has errors.'),
+        ),
+      );
+    } else {
+      form.save();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Form submitted.'),
+        ),
+      );
+    }
+  }
+
+  String? _validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Name is required.';
+    }
+    final nameExp = RegExp(r'^[A-Za-z ]+$');
+    if (!nameExp.hasMatch(value)) {
+      return 'Please enter only alphabetical characters.';
+    }
+    return null;
+  }
+
+  String? _validatePhoneNumber(String? value) {
+    final phoneExp = RegExp(r'^\d\d\d\d\d\d\d\d\d\d$');
+    if (!phoneExp.hasMatch(value!)) {
+      return 'Please enter a valid phone number.';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    final passwordField = _passwordFieldKey.currentState!;
+    if (passwordField.value == null || passwordField.value!.isEmpty) {
+      return 'Please enter a password.';
+    }
+    if (passwordField.value != value) {
+      return 'Passwords do not match.';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Form(
+      key: _formKey,
+      autovalidateMode: AutovalidateMode.values[_autoValidateModeIndex.value],
+      child: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: <Widget>[
+          TextFormField(
+            textCapitalization: TextCapitalization.words,
+            decoration: InputDecoration(labelText: 'Name'),
+            validator: _validateName,
+            onSaved: (String? value) {
+              person.name = value;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Phone Number'),
+            validator: _validatePhoneNumber,
+            keyboardType: TextInputType.phone,
+            onSaved: (String? value) {
+              person.phoneNumber = value;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Email'),
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (String? value) {
+              person.email = value;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Password'),
+            validator: _validatePassword,
+            onSaved: (String? value) {
+              person.password = value!;
+            },
+            obscureText: true,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Retype Password'),
+            validator: _validatePassword,
+            obscureText: true,
+          ),
+          SizedBox(
+            height: height * 0.1,
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                Color.fromARGB(255, 138, 205,
+                    156), // Adjusted opacity to 255 for full opacity
+              ),
+            ),
+            onPressed: _handleSubmitted,
+            child: Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// Format incoming numeric text to fit the format of (###) ###-####
+class _UsNumberTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final newTextLength = newValue.text.length;
+    final newText = StringBuffer();
+    var selectionIndex = newValue.selection.end;
+    var usedSubstringIndex = 0;
+    if (newTextLength >= 1) {
+      newText.write('(');
+      if (newValue.selection.end >= 1) selectionIndex++;
+    }
+    if (newTextLength >= 4) {
+      newText.write('${newValue.text.substring(0, usedSubstringIndex = 3)}) ');
+      if (newValue.selection.end >= 3) selectionIndex += 2;
+    }
+    if (newTextLength >= 7) {
+      newText.write('${newValue.text.substring(3, usedSubstringIndex = 6)}-');
+      if (newValue.selection.end >= 6) selectionIndex++;
+    }
+    if (newTextLength >= 11) {
+      newText.write('${newValue.text.substring(6, usedSubstringIndex = 10)} ');
+      if (newValue.selection.end >= 10) selectionIndex++;
+    }
+    // Dump the rest.
+    if (newTextLength >= usedSubstringIndex) {
+      newText.write(newValue.text.substring(usedSubstringIndex));
+    }
+    return TextEditingValue(
+      text: newText.toString(),
+      selection: TextSelection.collapsed(offset: selectionIndex),
+    );
+  }
+}
+
+class SignUpText extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const SignUpText({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: RichText(
+        text: TextSpan(
+          text: "Don't have an account? ",
+          style: TextStyle(color: Colors.black),
+          children: [
+            TextSpan(
+              text: "Sign up",
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/page2.dart
+++ lib/page2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:get/route_manager.dart';
+import 'package:safetravel/page3.dart';
 
 class MyPage2 extends StatelessWidget {
   const MyPage2({super.key});
@@ -10,145 +11,173 @@ class MyPage2 extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return Center(
       child: Scaffold(
+        appBar: AppBar(
+          title: Text('App Bar Example'),
+        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 300, // Width of the rectangle
-                height: 200,
-
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 213, 232, 224),
-                  // Color of the rectangle
-                  borderRadius: BorderRadius.circular(
-                      10), // Optional: You can add border radius for rounded corners
-                ), // Height of the rectangle
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 30,
-                        ),
-                        Image.asset('circle.png', width: 17, height: 20),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "Choose Current Location",
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Container(
-                      width: 250,
-                      height: 30,
-                      child: TextField(
-                        textAlign: TextAlign.start, // or TextAlign.left
-                        textAlignVertical: TextAlignVertical.center,
-                        cursorHeight: 20, // Adjust as needed
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Color.fromARGB(255, 247, 249, 248),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 30,
-                        ),
-                        Image.asset('map-pin.png', width: 20, height: 20),
-                        SizedBox(
-                          width: 0,
-                        ),
-                        Text("Choose Destination"),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Container(
-                      width: 250,
-                      height: 30,
-                      child: TextField(
-                        textAlign: TextAlign.start, // or TextAlign.left
-                        textAlignVertical: TextAlignVertical.center,
-                        cursorHeight: 20, // Adjust as needed
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Color.fromARGB(255, 247, 249, 248),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: height * 0.04,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 25,
-                width: 150,
-                child: OutlinedButton(
-                  onPressed: () {
-                    // Add onPressed callback function here
-                  },
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Color.fromARGB(255, 0, 0, 0),
-                    backgroundColor:
-                        Color.fromARGB(255, 255, 255, 255), // Text color
-                    side: BorderSide(
-                        width: 0.5, color: Colors.black), // Outline stroke
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(30.0), // Circular border radius
+                Container(
+                  width: width * 0.6, // Width of the rectangle
+                  height: height * 0.4,
+
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 213, 232, 224),
+                    // Color of the rectangle
+                    borderRadius: BorderRadius.circular(
+                        30), // Optional: You can add border radius for rounded corners
+                  ), // Height of the rectangle
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: width * 0.1,
+                          ),
+                          Image.asset('circle.png', width: 17, height: 20),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Choose Current Location",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0), // Text color
+                              fontSize: 18,
+                              fontFamily: 'Inter.ttf', // Font size
+                              fontWeight: FontWeight.normal, // Font weight
+                            ),
+                          ),
+                          SizedBox(
+                            width: width * 0.1,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * 0.019,
+                      ),
+                      Container(
+                        width: width * 0.4,
+                        height: height * 0.05,
+                        child: TextField(
+                          textAlign: TextAlign.start, // or TextAlign.left
+                          textAlignVertical: TextAlignVertical.center,
+                          cursorHeight: 20, // Adjust as needed
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 247, 249, 248),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.05,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: width * 0.1,
+                          ),
+                          Image.asset('map-pin.png', width: 20, height: 20),
+                          SizedBox(
+                            width: 0,
+                          ),
+                          Text(
+                            "Choose Destination",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0), // Text color
+                              fontSize: 18,
+                              fontFamily: 'Inter.ttf', // Font size
+                              fontWeight: FontWeight.normal, // Font weight
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      Container(
+                        width: width * 0.4,
+                        height: height * 0.05,
+                        child: TextField(
+                          textAlign: TextAlign.start, // or TextAlign.left
+                          textAlignVertical: TextAlignVertical.center,
+                          cursorHeight: 20, // Adjust as needed
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 247, 249, 248),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.01,
+                ),
+                Container(
+                  height: height * 0.05,
+                  width: width * 0.5,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyPage3()),
+                      );
+                    },
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                      backgroundColor:
+                          Color.fromARGB(255, 186, 209, 205), // Text color
+                      side: BorderSide(
+                          width: 0.5, color: Colors.black), // Outline stroke
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            30.0), // Circular border radius
+                      ),
+                    ),
+                    child: Text(
+                      "go",
+                      style: TextStyle(fontSize: 18),
                     ),
                   ),
-                  child: Text("go"),
                 ),
-              ),
 
-              SizedBox(
-                height: 30,
-              ),
-
-              Container(
-                width: width * 0.4,
-                height: height * 0.4,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Image.asset('LOGO.png', width: 200, height: 150),
-                    const SizedBox(height: 10),
-                    Text("Your safest travel partner"),
-                  ],
+                SizedBox(
+                  height: height * 0.001,
                 ),
-              ),
 
-              // Adjust as needed
-            ],
+                Container(
+                  width: width * 0.8,
+                  height: height * 0.4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset('LOGO.png',
+                          width: width * 0.5, height: width * 0.4),
+                      SizedBox(
+                        height: height * 0.001,
+                      ),
+                      Text("Your safest travel partner"),
+                    ],
+                  ),
+                ),
+
+                // Adjust as needed
+              ],
+            ),
           ),
         ),
       ),
