071d51ea84f8346e044b704babce53a8927c29f3
Krishnaraju06 <krishnapriyaraju07@gmail.com>
2024-04-13 22:51:00 +0530

    auth

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,1 +1,26 @@
+// ignore_for_file: unused_import
+
 import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:safetravel/firebase_options.dart';
 // import 'package:loginsignup/login.dart';
 import 'package:safetravel/login.dart';
 import "package:safetravel/splash.dart";
 import 'package:safetravel/page2.dart';
 import 'package:safetravel/page3.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'splash',
+    initialRoute: 'login',
     routes: {
       'login': (BuildContext) => MyLogin(),
       'page2': (BuildContext) => MyPage2(),
