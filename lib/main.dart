41119a8a074bdc9fa9d355427b132f07df7a0253
aravinds <9aravinds@gmail.com>
2024-04-16 16:09:03 +0530

    t

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:safetravel/firebase_options.dart';
-// import 'package:loginsignup/login.dart';
 import 'package:safetravel/login.dart';
 import "package:safetravel/splash.dart";
 import 'package:safetravel/page2.dart';
