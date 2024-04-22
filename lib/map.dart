d52bb89fa0e09e7a3849ffe11b048ce7c948b7a7
Deepthi1818 <deepthideepz883@gmail.com>
2024-04-22 18:18:36 +0530

    Added mapbox API

--- lib/map.dart
+++ lib/map.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter/material.dart'
 
 void main()
@@ -17,15 +18,44 @@ class MapApp extends StatelessWidget{
 
 class MyHomePage extends StatefulWidget {
   @override
+_MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+
       ),
-      body: 
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.5, -0.09),
+          zoom: 13.0,
+        ),
+        layers: [
+          TileLayerOptions(
+            utlTemplater: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a','b','c']
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: LatLng(51.5, -0.09),
+                builder: (ctx) =>
+                Container(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
     )
   }
 }
-
 
 
