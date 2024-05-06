8a4828707f1e93efd636f67dfcd02025993c04e3
WavBoi <emmanueljose003@gmail.com>
2024-05-07 01:04:41 +0530

    added new map file to it

---
+++ lib/map_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: _pGooglePlex,
+            zoom: 20,
+          ),
+          markers: {
+            Marker(
+                markerId: MarkerId("currentLocation"),
+                icon: BitmapDescriptor.defaultMarker,
+                position: _pGooglePlex),
+            Marker(
+                markerId: MarkerId("currentLocation"),
+                icon: BitmapDescriptor.defaultMarker,
+                position: _pGooglePlex)
+          }),
+    );
+  }
+}
